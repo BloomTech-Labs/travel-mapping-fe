09a9d308c74b64b7b3198aa7b8291b489babc4a9
ericlugo <ericlugo@users.noreply.github.com>
2019-11-06 01:58:47 -0500

    created base UI for overview panels. to be extended as necessary.
    - remove imageCard as sample no longer necessary.
    - modify LandingPage to just include a HelloWorld temporarily.
    - modify App.js to include new temporary route.
    - create NavBar and ActionButton Components to be extended later.
    - create Main view as base for Album and App overview viewports.

--- piktorlog/src/App.js
+++ piktorlog/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './views/Landing';
+import Main from './views/Main';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={LandingPage} />
+      <Route exact path='/main' component={Main} />
     </Router>
   );
 }
