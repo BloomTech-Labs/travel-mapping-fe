6801a433b3c7e588c6f04f0b63e8a5b64d4bd48e
brandon-elzy <brandon.g.elzy@line-zero.com>
2019-11-07 19:16:35 -0600

    Configured exception monitoring

--- piktorlog/src/App.js
+++ piktorlog/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './views/Landing';
+
+// Initialize Sentry.io for exception monitoring.
+Sentry.init({dsn: "https://2ec319603fba4c4aa4d8bc7f56b40e33@sentry.io/1811913"});
+
+const environment = process.env.NODE_ENV || 'development';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={LandingPage} />
+      { 
+        /* Used to verify Sentry integration (development only) */
+        environment === 'development' && <button onClick={() => new Error('Sentry Error') }>Break the world</button>
+      }
     </Router>
   );
 }
