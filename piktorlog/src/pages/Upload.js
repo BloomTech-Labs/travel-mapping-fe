f1f7f2ebf1cdc4a6db82943706759a820863e928
Tim Dill <brodt7258@gmail.com>
2019-11-13 22:51:52 -0500

    bring in standard navbar

--- piktorlog/src/pages/Upload.js
+++ piktorlog/src/pages/Upload.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavBar from '../components/organisms/NavBar';
 
 const UploadPage = () => {
+  const [activeNavItem, setActiveNavItem] = useState('');
+  const handleNavItemClick = (e, { name }) => setActiveNavItem(name);
+
   return (
     <React.Fragment>
+      <NavBar
+        header='Piktorlog'
+        activeItem={activeNavItem}
+        handleClick={handleNavItemClick}
+      />
       <div>Upload photos</div>
     </React.Fragment>
   );
