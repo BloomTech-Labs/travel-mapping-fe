87b9258cbd621ca18293b3b1619b7393027de973
arturolei <artie.lei@gmail.com>
2019-11-18 01:43:57 -0800

    Added useEffect to Main.js, not sure if we will need it, but better to be safe. I also wanted to make a git push and git commit while high in the sky.

--- piktorlog/src/pages/AlbumsOverview.js
+++ piktorlog/src/pages/AlbumsOverview.js
@@ -4,10 +4,23 @@ import NavBar from '../components/organisms/NavBar';
 import ActionButton from '../components/organisms/ActionButton';
 import AlbumsList from '../components/organisms/AlbumsList';
 
-const AlbumsOverview = () => {
+const AlbumsOverview = (props) => {
     const [activeNavItem, setActiveNavItem] = useState('');
     const [actionToggle, setActionToggle] = useState(false);
-    // const history = useHistory();
+    //const history = useHistory();
+
+    /*
+    Need to develop code for handling album changes or prmpting changes
+    const [albumData, getAlbumData] = useState([]);
+
+    useEffect(()=>{
+      function handleAlbumStatusChange(status){
+        getAlbumData(status.albumData);
+      }
+
+    });
+
+    */
   
     const handleNavItemClick = (e, { name }) => setActiveNavItem(name);
     const handleButtonClick = () => setActionToggle(!actionToggle);
