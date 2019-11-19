27be37a12973ef3e7d64266360d3c4ee5a76cfc2
adrian <adrianadames@gmail.com>
2019-11-19 17:48:15 -0500

    Set up form for entering the album name, description, and metadata of a new album user wants to create. Input values to album name, description, and metadata held in local state via hooks. Started connecting the component to our redux store, but have not yet made the actions and action creators that will be fired off when user clicks on the Create button or the Cancel button.

--- piktorlog/src/pages/CreateAlbum.js
+++ piktorlog/src/pages/CreateAlbum.js
@@ -1,1 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createAlbum } from '../store/actions'
+
+const CreateAlbum = (props) => {
+    console.log('props: ', props);
+
+    const createAlbum = () => {
+        props.createAlbum();
+    }
+
+    const [albumName, setAlbumName] = useState('');
+    const [albumDescription, setAlbumDescription] = useState('');
+    const [albumMetadata, setAlbumMetadata] = useState([]);
+
+    return (
+        <>
+            <h1>CREATE ALBUM PAGE</h1>
+            <form>
+            
+                <label>
+                    Album Name:
+                <input 
+                    type = 'text'
+                    name = 'albumName'
+                    value = {albumName}
+                    onChange = {e => setAlbumName(e.target.value)}
+                />
+                </label>
+
+                <br />
+            
+                <label>
+                    Album Description:
+                <input 
+                    type = 'text'
+                    name = 'albumDescription'
+                    value = {albumDescription}
+                    onChange = {e => setAlbumDescription(e.target.value)}
+                />
+                </label>
+
+                <br />
+            
+                <label>
+                    Album Metadata:
+                <input 
+                    type = 'text'
+                    name = 'albumMetadata'
+                    value = {albumMetadata}
+                    onChange = {e => setAlbumMetadata(e.target.value)}
+                />
+                </label>
+
+                <br />
+
+                <button
+                    type = 'button'
+                    // onClick = {props.createAlbum()}
+                >
+                    Create
+                </button>
+
+                <br />
+
+                <button
+                    type = 'button'
+                >
+                    Cancel
+                </button>
+                
+            </form>
+
+        </>
+    )
+};
+
+const mapDispatchToProps = {
+    createAlbum
+};
+
+export default connect(
+    state => state,
+    mapDispatchToProps
+)(CreateAlbum);
