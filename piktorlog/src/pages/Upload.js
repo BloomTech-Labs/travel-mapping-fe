5f6f2147fde79af90ac4195448a3c2b3156c15d9
Tim Dill <brodt7258@gmail.com>
2019-11-18 20:45:05 -0500

    setup dropzone and post to server

--- piktorlog/src/pages/Upload.js
+++ piktorlog/src/pages/Upload.js
@@ -1,15 +1,28 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import NavBar from '../components/organisms/NavBar';
+import axios from 'axios';
 
 const UploadPage = () => {
   const [activeNavItem, setActiveNavItem] = useState('');
   const handleNavItemClick = (e, { name }) => setActiveNavItem(name);
 
   const [images, setImages] = useState([]);
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles);
+    setImages(prev => [...prev, ...acceptedFiles]);
+  }, []);
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
 
-  const handleImageSelect = e => {
-    setImages([...images, ...e.target.files]);
+  const [albums, setAlbums] = useState([]);
+  useEffect(() => {
+    // query server for currentUser's albums
+  }, []);
+
+  const handleSubmit = () => {
+    axios.post('http://localhost:4000', {
+      images,
+    })
   };
 
   console.log(images);
@@ -21,17 +34,22 @@ const UploadPage = () => {
         handleClick={handleNavItemClick}
       />
       <div>Upload photos</div>
-      <form action="">
-        <input 
-          type="file"
-          onChange={handleImageSelect}
-        />
-      </form>
-      {
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
+      <button onClick={handleSubmit}>
+        Submit
+      </button>
+      {/* {
         images.map((e, i) => (
           <img key={i} src={URL.createObjectURL(e)} alt="" />
         ))
-      }
+      } */}
     </React.Fragment>
   );
 };
