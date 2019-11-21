6ab2e41223b067607523ce0b16052f42da6cb2cf
Tim Dill <brodt7258@gmail.com>
2019-11-21 01:08:04 -0500

    cleanup and comments for login buttons

--- piktorlog/src/App.js
+++ piktorlog/src/App.js
@@ -6,9 +6,6 @@ import Main from './pages/Main';
 import login from './store/actions/login';
 import logout from './store/actions/logout';
 import { connect } from 'react-redux';
-// import axios from 'axios';
-// import address from './serverAddress';
-// import ls from 'local-storage';
 import checkLogin from './store/actions/checkLogin';
 
 // Initialize Sentry.io for exception monitoring.
@@ -16,20 +13,26 @@ Sentry.init({dsn: "https://2ec319603fba4c4aa4d8bc7f56b40e33@sentry.io/1811913"})
 
 const environment = process.env.NODE_ENV || 'development';
 
-// axios.get(`${address}/users`).then(({ data }) => console.log(data));
+// All the login stuff is here for convenience during development/testing.
+// Anyone who wants to move or delete it, feel free.
 
 function App({ login, logout, checkLogin }) {
   
   useEffect(() => {
-    // const user = ls.get('user');
-    // console.log('LS', user);
     checkLogin();
   }, [checkLogin]);
+
+  const handleLogin = () => {
+    const email = 'test1@test1.com';      // Replace both of these with the credentials of a
+    const password = 'ThisIsATest#1234';  // user in the database you're developing with
+
+    login(email, password);
+  };
 
   return (
     
     <Router>
-      <button onClick={() => login('test1@test1.com', 'ThisIsATest#1234')}>login</button>
+      <button onClick={handleLogin}>login</button>
       <button onClick={logout}>logout</button>
       <Route exact path='/' component={LandingPage} />
       <Route exact path='/main' component={Main} />
