07eddebee8c88c0d3b2af755440a2ff6a701c870
adrian <adrianadames@gmail.com>
2019-11-26 01:48:23 -0500

    In comments I made a first pass at drafting the shape of our state. Drafted code for the reducer switch case of CREATE_ALBUM_SUCCESS. Next step is to test if the reducer works as expected by firing off the CREATE_ALBUM_REQUEST action in the CreateAlbum component.

--- piktorlog/src/store/reducers/albumsReducer.js
+++ piktorlog/src/store/reducers/albumsReducer.js
@@ -4,62 +4,139 @@ import {
     CREATE_ALBUM_FAILURE,
 } from '../actions/types';
 
+// // What should our state look like here? Let me take a stab at it. 
+// // (important reference: https://redux.js.org/recipes/structuring-reducers/normalizing-state-shape)
+// const initialState = {
+//   currentUser: {}, 
+//   entities: {
+//     userAlbums: {
+//       byId: {
+//         albumId1: {
+//           id: albumdId1,
+//           user_id: action.payload.user_id,
+//           title: action.payload.title,
+//           description: action.payload.description, 
+//           access: action.payload.access, 
+//           created_at: action.payload.created_at,
+//           updated_at: action.payload.updated_at, 
+//           meta: {}
+//         },
+//         albumId2: {
+//           id: albumdId2,
+//           user_id: action.payload.user_id,
+//           title: action.payload.title,
+//           description: action.payload.description, 
+//           access: action.payload.access, 
+//           created_at: action.payload.created_at,
+//           updated_at: action.payload.updated_at, 
+//           meta: {}
+//         }
+//       },
+//       allIds: [albumId1, albumId2]
+//     },
+//     userMedia: {
+//       byId: {
+//         mediaId1: {
+//           id: mediaId1,
+//           user_id: val, 
+//           title: val, 
+//           caption:val, 
+//           type: val, 
+//           media_url: val, 
+//           created_at: val,
+//           updated_at: val, 
+//         }, 
+//         mediaId2: {
+//           id: mediaId2,
+//           user_id: val, 
+//           title: val, 
+//           caption:val, 
+//           type: val, 
+//           media_url: val, 
+//           created_at: val,
+//           updated_at: val, 
+//         }
+//       }, 
+//       allIds: [mediaId1, mediaId2]
+//     },
+//     mediaAlbums: {
+//       byId: {
+//         1: {
+//           id:1, 
+//           albumId: albumIdVal, 
+//           mediaId: mediaIdVal
+//         }, 
+//         2: {
+//           id:2, 
+//           albumId: albumIdVal, 
+//           mediaId: mediaIdVal 
+//         }, 
+//         3: {
+//           id:3, 
+//           albumId:albumId1, 
+//           mediaId:mediaId1
+//         }, 
+//         4: {
+//           id:4, 
+//           albumId:albumId1, 
+//           mediaId:mediaId2
+//         }, 
+//         5: {
+//           id:5, 
+//           albumId: albumId2, 
+//           mediaId: mediaId2
+//         },
+//         6: {
+//           id:6, 
+//           albumId: albumId2, 
+//           mediaId: mediaId2
+//         }
+//       }
+//     }
+//   }
+// }
+
+// ok cool. I have an idea of what the state will look like. 
+const initialState = {
+  currentUser: {}, 
+  entities: {
+    userAlbums: {
+      byId: {}, 
+      allIds: []
+    }, 
+    userMedia: {
+      byId: {}, 
+      allIds: []
+    },
+    mediaAlbums: {
+      byId: {}, 
+      allIds: []
+    }
+  }
+} 
+
 // NOTE: in the switch statement below, should I include the case
 // when the type is CREATE_ALBUM_REQUEST? I noticed the analog to 
 // this in the userReducer did not include a case for 
 // REGISTER_REQUEST, LOGIN_REQUEST, and NOT_YET_LOGGED_IN
 
-// const initialState = {
-//     albumTitle: '', 
-//     albumDescription:'', 
-//     albumMetadata: [{}],
-// }
-
-
-// the payload for the CREATE_ALBUM_SUCCESS action type is of the following form:
-// {
-//     "album_id": 0,
-//     "user_id": 6534,
-//     "title": "Vacation Photos",
-//     "description": "Awesome fun vacation time in the Mexico with all the friends",
-//     "access": "public",
-//     "created_at": "2019-11-17 03:02:35",
-//     "updated_at": "2019-11-17 03:02:35",
-//     "meta": {}
-// }
-
-
-export default (state = {}, action) => {
+// NOTE: not sure how to go about handling errors here in the reducer. 
+export default (state = initialState, action) => {
     switch(action.type) {
-        // // leaving out the case commented out below for now
-
-        // case CREATE_ALBUM_REQUEST:
-        //   return {
-        //     ...state,
-        //     error: '', 
-        //     createAlbumRequest: true
-        //   };
-
-        case CREATE_ALBUM_SUCCESS:
-          console.log('action.payload: ',  action.payload);
-        // the payload for the CREATE_ALBUM_SUCCESS action type is of the following form:
-        // {
-        //     "album_id": 0,
-        //     "user_id": 6534,
-        //     "title": "Vacation Photos",
-        //     "description": "Awesome fun vacation time in the Mexico with all the friends",
-        //     "access": "public",
-        //     "created_at": "2019-11-17 03:02:35",
-        //     "updated_at": "2019-11-17 03:02:35",
-        //     "meta": {}
-        // }
+        case CREATE_ALBUM_REQUEST:
           return {
             ...state,
-            // 
-
+          };
+        case CREATE_ALBUM_SUCCESS:
+          console.log('currentState: ', state);
+          const newState = {...state};
+          newState.entities.userAlbums.byId[action.payload.album_id] = action.payload;
+          newState.entities.userAlbums.allIds.push(action.payload.album_id);
+          console.log('updatedState: ', newState);
+          return {
+            ...newState
           };
         case CREATE_ALBUM_FAILURE:
-          console.log('action.payload: ',  action.payload);
           return {
             ...state,
           };
@@ -67,4 +144,3 @@ export default (state = {}, action) => {
           return state;
     } 
 }
-  
