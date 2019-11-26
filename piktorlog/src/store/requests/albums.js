938c7abe0da4d9381cc57c9b8e967ec528b8a363
adrian <adrianadames@gmail.com>
2019-11-25 23:14:53 -0500

    Moved a couple of the functions in this file so that order of functions is in the same order in which I'll be using them when testing the actions and reducer for a user's albums.

--- piktorlog/src/store/requests/albums.js
+++ piktorlog/src/store/requests/albums.js
@@ -2,16 +2,6 @@ import axios from 'axios';
 
 import { address, createAuthHeader } from '../utils';
 
-// album_id is an integer
-// metaData is an array of objects in the form { name: <String>, value: <String> }
-export const addAlbumMeta = async (album_id, metaData) => {
-  const header = createAuthHeader();
-  if (!header) return false;
-
-  const { data } = await axios.post(`${address}/albums/${album_id}/meta/add`, metaData, header);
-  return data;
-};
-
 export const createAlbum = async (user_id, title, description, access = 'public') => {
   const header = createAuthHeader();
   if (!header) return false;
@@ -19,6 +9,17 @@ export const createAlbum = async (user_id, title, description, access = 'public'
   const { data } = await axios.post(
     `${address}/users/${user_id}/albums/create`,
     { title, description, access },
+    header
+  );
+  return data;
+};
+
+export const getUserAlbums = async (user_id) => {
+  const header = createAuthHeader();
+  if (!header) return false;
+
+  const { data } = await axios.get(
+    `${address}/users/${user_id}/albums`,
     header
   );
   return data;
@@ -37,17 +38,6 @@ export const editAlbum = async (album_id, changes) => {
   return data;
 };
 
-export const getUserAlbums = async (user_id) => {
-  const header = createAuthHeader();
-  if (!header) return false;
-
-  const { data } = await axios.get(
-    `${address}/users/${user_id}/albums`,
-    header
-  );
-  return data;
-};
-
 export const deleteAlbum = async (album_id) => {
   const header = createAuthHeader();
   if (!header) return false;
@@ -56,5 +46,15 @@ export const deleteAlbum = async (album_id) => {
     `${address}/albums/${album_id}/remove`,
     header
   );
+  return data;
+};
+
+// album_id is an integer
+// metaData is an array of objects in the form { name: <String>, value: <String> }
+export const addAlbumMeta = async (album_id, metaData) => {
+  const header = createAuthHeader();
+  if (!header) return false;
+
+  const { data } = await axios.post(`${address}/albums/${album_id}/meta/add`, metaData, header);
   return data;
 };
