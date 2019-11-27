3549fdaa7899cda5f08fe69acbeae0a4ef13caa6
adrian <adrianadames@gmail.com>
2019-11-26 21:04:48 -0500

    Changed names of functions that do crud commands by appending Req to the name of each one.

--- piktorlog/src/store/requests/albums.js
+++ piktorlog/src/store/requests/albums.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { address, createAuthHeader } from '../utils';
 
-export const createAlbum = async (user_id, title, description, access = 'public') => {
+export const createAlbumReq = async (user_id, title, description, access = 'public') => {
   const header = createAuthHeader();
   if (!header) return false;
 
@@ -14,7 +14,7 @@ export const createAlbum = async (user_id, title, description, access = 'public'
   return data;
 };
 
-export const getUserAlbums = async (user_id) => {
+export const getUserAlbumsReq = async (user_id) => {
   const header = createAuthHeader();
   if (!header) return false;
 
@@ -26,7 +26,7 @@ export const getUserAlbums = async (user_id) => {
 };
 
 // changes is an object with the optional String properties: 'title', 'description', and 'access'
-export const editAlbum = async (album_id, changes) => {
+export const editAlbumReq = async (album_id, changes) => {
   const header = createAuthHeader();
   if (!header) return false;
 
@@ -38,7 +38,7 @@ export const editAlbum = async (album_id, changes) => {
   return data;
 };
 
-export const deleteAlbum = async (album_id) => {
+export const deleteAlbumReq = async (album_id) => {
   const header = createAuthHeader();
   if (!header) return false;
 
@@ -51,7 +51,7 @@ export const deleteAlbum = async (album_id) => {
 
 // album_id is an integer
 // metaData is an array of objects in the form { name: <String>, value: <String> }
-export const addAlbumMeta = async (album_id, metaData) => {
+export const addAlbumMetaReq = async (album_id, metaData) => {
   const header = createAuthHeader();
   if (!header) return false;
 
