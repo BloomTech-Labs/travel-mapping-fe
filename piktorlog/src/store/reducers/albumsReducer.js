99d79cae845d5dcb80e6b874bdbb7c3aff29bff9
adrian <adrianadames@gmail.com>
2019-11-26 22:54:21 -0500

    Removed long comment from this file regarding the shape of our state tree and moved that to our slack channel for discussion. Also edited a question I have as a comment regarding how we should go about handling errors that come about for different action types and whatnot.

--- piktorlog/src/store/reducers/albumsReducer.js
+++ piktorlog/src/store/reducers/albumsReducer.js
@@ -4,115 +4,10 @@ import {
     CREATE_ALBUM_FAILURE,
 } from '../actions/types';
 
-// // What should our state look like here? Let me take a stab at it. 
-// // (important reference: https://redux.js.org/recipes/structuring-reducers/normalizing-state-shape)
-// const initialState = {
-//   currentUser: {}, 
-//   entities: {
-//     userAlbums: {
-//       byId: {
-//         albumId1: {
-//           id: albumdId1,
-//           user_id: action.payload.user_id,
-//           title: action.payload.title,
-//           description: action.payload.description, 
-//           access: action.payload.access, 
-//           created_at: action.payload.created_at,
-//           updated_at: action.payload.updated_at, 
-//           meta: {}
-//         },
-//         albumId2: {
-//           id: albumdId2,
-//           user_id: action.payload.user_id,
-//           title: action.payload.title,
-//           description: action.payload.description, 
-//           access: action.payload.access, 
-//           created_at: action.payload.created_at,
-//           updated_at: action.payload.updated_at, 
-//           meta: {}
-//         }
-//       },
-//       allIds: [albumId1, albumId2]
-//     },
-//     userMedia: {
-//       byId: {
-//         mediaId1: {
-//           id: mediaId1,
-//           user_id: val, 
-//           title: val, 
-//           caption:val, 
-//           type: val, 
-//           media_url: val, 
-//           created_at: val,
-//           updated_at: val, 
-//         }, 
-//         mediaId2: {
-//           id: mediaId2,
-//           user_id: val, 
-//           title: val, 
-//           caption:val, 
-//           type: val, 
-//           media_url: val, 
-//           created_at: val,
-//           updated_at: val, 
-//         }
-//       }, 
-//       allIds: [mediaId1, mediaId2]
-//     },
-//     mediaAlbums: {
-//       byId: {
-//         1: {
-//           id:1, 
-//           albumId: albumIdVal, 
-//           mediaId: mediaIdVal
-//         }, 
-//         2: {
-//           id:2, 
-//           albumId: albumIdVal, 
-//           mediaId: mediaIdVal 
-//         }, 
-//         3: {
-//           id:3, 
-//           albumId:albumId1, 
-//           mediaId:mediaId1
-//         }, 
-//         4: {
-//           id:4, 
-//           albumId:albumId1, 
-//           mediaId:mediaId2
-//         }, 
-//         5: {
-//           id:5, 
-//           albumId: albumId2, 
-//           mediaId: mediaId2
-//         },
-//         6: {
-//           id:6, 
-//           albumId: albumId2, 
-//           mediaId: mediaId2
-//         }
-//       }
-//     }
-//   }
-// }
-
-// ok cool. I have an idea of what the state will look like. 
 const initialState = {
-  entities: {
-    userAlbums: {
-      byId: {}, 
-      allIds: []
-    }, 
-    userMedia: {
-      byId: {}, 
-      allIds: []
-    },
-    mediaAlbums: {
-      byId: {}, 
-      allIds: []
-    }
-  }
-} 
+  byId: {},
+  allIds: []
+}; 
 
 // NOTE: in the switch statement below, should I include the case
 // when the type is CREATE_ALBUM_REQUEST? I noticed the analog to 
@@ -120,6 +15,9 @@ const initialState = {
 // REGISTER_REQUEST, LOGIN_REQUEST, and NOT_YET_LOGGED_IN
 
 // NOTE: not sure how to go about handling errors here in the reducer. 
+// Should we do something like having an error field in the albums
+// reducer that gets cleared or populated for actions successes or
+// failures, respectively?
 export default (state = initialState, action) => {
     switch(action.type) {
         case CREATE_ALBUM_REQUEST:
@@ -127,13 +25,13 @@ export default (state = initialState, action) => {
             ...state,
           };
         case CREATE_ALBUM_SUCCESS:
-          console.log('currentState: ', state);
-          const newState = {...state};
-          newState.entities.userAlbums.byId[action.payload.album_id] = action.payload;
-          newState.entities.userAlbums.allIds.push(action.payload.album_id);
-          console.log('updatedState: ', newState);
           return {
-            ...newState
+            ...state,
+            byId: {
+              ...state.byId, 
+              [action.payload.album_id]:action.payload
+            }, 
+            allIds: [...state.allIds, action.payload.album_id]
           };
         case CREATE_ALBUM_FAILURE:
           return {
