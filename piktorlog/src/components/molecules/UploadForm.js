cd77de6d0c1b29f5a33bf6079639857f27fe44f8
Tim Dill <brodt7258@gmail.com>
2019-11-27 20:37:33 -0500

    build out the individual uploading forms

--- piktorlog/src/components/molecules/UploadForm.js
+++ piktorlog/src/components/molecules/UploadForm.js
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react';
-import { Segment, Image, Grid } from 'semantic-ui-react';
+import { Segment, Image, Grid, Form, Button } from 'semantic-ui-react';
 
-const UploadForm = ({ media }) => {
+const UploadForm = ({ media, setMedia }) => {
 
   useEffect(() => {
     console.log('UF', media);
   }, [media]);
 
+  const handleChange = (e, { name, value }) => {
+    setMedia(media.id, { [name]: value });
+  };
+
+  const clear = () => {
+    setMedia(media.id, {
+      title: '',
+      caption: '',
+      keywords: ''
+    });
+  };
 
   return (
     <Segment>
@@ -14,8 +25,38 @@ const UploadForm = ({ media }) => {
         <Grid.Column width="6">
           <Image src={URL.createObjectURL(media.file)} rounded />
         </Grid.Column>
-        <Grid.Column width="10">
-          
+        <Grid.Column width="9">
+          <Form>
+            <Form.Input
+              fluid
+              name="title"
+              placeholder="title"
+              value={media.title}
+              onChange={handleChange}
+            />
+            <Form.Input
+              fluid
+              name="caption"
+              placeholder="caption"
+              value={media.caption}
+              onChange={handleChange}
+            />
+            <Form.Input
+              fluid
+              name="keywords"
+              placeholder="keywords (comma-separated, e.g. one, two, three)"
+              value={media.keywords}
+              onChange={handleChange}
+            />
+          </Form>
+          <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '1rem' }}>
+            <Button onClick={clear}>
+              Clear
+            </Button>
+            <Button>
+              Remove
+            </Button>
+          </div>
         </Grid.Column>
       </Grid>
     </Segment>
