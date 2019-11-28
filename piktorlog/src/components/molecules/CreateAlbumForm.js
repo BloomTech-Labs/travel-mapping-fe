1ad17c055e5698ae25d9691aa74e7c07c1d7764d
adrian <adrianadames@gmail.com>
2019-11-28 07:11:57 -0500

    Set up input fields for metadata and local state for holding all metadata key-value pairs. Metadata will be sent to createAlbum action.

--- piktorlog/src/components/molecules/CreateAlbumForm.js
+++ piktorlog/src/components/molecules/CreateAlbumForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Segment, Dropdown, Button, Header } from 'semantic-ui-react';
+import { Form, Segment, Dropdown, Button, Header, Grid, Container, GridColumn, Icon, Divider, List } from 'semantic-ui-react';
 
 const CreateAlbumForm = (props) => {
 
@@ -7,7 +7,18 @@ const CreateAlbumForm = (props) => {
     const [description, setDescription] = useState('');
     const [access, setAccess] = useState('public');
 
-    const submit = () => {
+    const [metaFieldName, setMetaFieldName] = useState('');
+    const [metaFieldValue, setMetaFieldValue] = useState('');
+    const [metadata, setMetaData] = useState([]);
+
+    const addMeta = (metaFieldName, metaFieldValue) => {
+        metadata.push({[metaFieldName]: metaFieldValue});
+        setMetaFieldName('');
+        setMetaFieldValue('');
+        console.log('metadata: ', metadata)
+    }
+    
+    const createAlbum = () => {
         props.createAlbum(props.user_id, title, description, access);
     };
 
@@ -17,6 +28,7 @@ const CreateAlbumForm = (props) => {
                 Create Album
             </Header>
             <Segment>
+                <Segment>
                 <Form.Input 
                     fluid
                     placeholder = 'title'
@@ -29,7 +41,7 @@ const CreateAlbumForm = (props) => {
                     value = {description}
                     onChange = {e => setDescription(e.target.value)}
                 />
-                Access
+                
                 <Form.Dropdown
                     options = {[
                         {key:'public', value:'public', text:'public'}, 
@@ -37,10 +49,69 @@ const CreateAlbumForm = (props) => {
                     ]}
                     selection      
                     value = {access}
-                    // onClick = {e => setAccess(e.target.value)}
                     onChange = {(e,data) => setAccess(data.value)}
                 />
-                <Button color = 'teal' onClick  = {submit}>
+                </Segment>
+                <Segment>
+                <Grid >
+                    <Header as = 'h5' color = 'teal' textAlign = 'left'>
+                        Add Metadata
+                    </Header>
+                    <Grid.Row colums = {3}>
+                        <GridColumn width = {7}>
+                            <Form.Input 
+                                fluid
+                                placeholder = 'Name'
+                                value = {metaFieldName}
+                                onChange = {e => setMetaFieldName(e.target.value)}
+                            />
+                        
+                        </GridColumn>
+                        <GridColumn width = {7}>
+                            <Form.Input 
+                                fluid
+                                placeholder = 'Value'
+                                value = {metaFieldValue}
+                                onChange = {e => setMetaFieldValue(e.target.value)}
+                            />
+                        </GridColumn>
+                        <GridColumn>
+                            <Button icon onClick = {e => addMeta(metaFieldName, metaFieldValue)}>
+                                <Icon name = 'plus circle' />
+                            </Button>
+                        </GridColumn>
+                    </Grid.Row>
+                    <Grid.Row colums = {1}>
+                        <GridColumn width = {16}>
+                            <List>
+                            {metadata.map((metadataObject, i) => {
+                                return (
+                                <List.Item key = {i}>
+                                    <List.Content>
+                                        <Grid>
+                                        <Grid.Row columns = {2}>
+                                            <GridColumn>
+                                                {Object.keys(metadataObject)[0]}
+                                            </GridColumn>
+                                            <GridColumn>
+                                                {Object.values(metadataObject)[0]}
+                                            </GridColumn>
+                                        </Grid.Row>
+                                        </Grid>
+                                    </List.Content>
+                                </List.Item>
+                                )
+                            })}
+                            </List>
+                        </GridColumn>
+                    </Grid.Row>
+
+                </Grid>  
+                </Segment>   
+                
+                
+                
+                <Button color = 'teal' onClick  = {createAlbum}>
                     Create
                 </Button>
                 <Button>
