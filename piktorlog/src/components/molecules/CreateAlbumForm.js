cdbf64fd375007b5faeb0cc2b5c9ed5839eb6780
adrian <adrianadames@gmail.com>
2019-11-28 04:14:21 -0500

    Refactored CreateAlbum page so that it imports a CreateAlbumForm component in the component molecules directory. Added a dropdown menu where user can select if album is to be public or private.

--- piktorlog/src/components/molecules/CreateAlbumForm.js
+++ piktorlog/src/components/molecules/CreateAlbumForm.js
@@ -1,1 +1,54 @@
 import React, { useState } from 'react';
+import { Form, Segment, Dropdown, Button, Header } from 'semantic-ui-react';
+
+const CreateAlbumForm = (props) => {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [access, setAccess] = useState('public');
+
+    const submit = () => {
+        props.createAlbum(props.user_id, title, description, access);
+    };
+
+    return (
+        <Form size = 'large'>
+            <Header as = 'h2' color = 'teal' textAlign = 'center'>
+                Create Album
+            </Header>
+            <Segment>
+                <Form.Input 
+                    fluid
+                    placeholder = 'title'
+                    value = {title}
+                    onChange = {e => setTitle(e.target.value)}
+                />
+                <Form.Input 
+                    fluid
+                    placeholder = 'description'
+                    value = {description}
+                    onChange = {e => setDescription(e.target.value)}
+                />
+                Access
+                <Form.Dropdown
+                    options = {[
+                        {key:'public', value:'public', text:'public'}, 
+                        {key:'private', value:'private', text:'private'}, 
+                    ]}
+                    selection      
+                    value = {access}
+                    // onClick = {e => setAccess(e.target.value)}
+                    onChange = {(e,data) => setAccess(data.value)}
+                />
+                <Button color = 'teal' onClick  = {submit}>
+                    Create
+                </Button>
+                <Button>
+                    Cancel
+                </Button>
+            </Segment>
+        </Form>
+    )
+}
+
+export default CreateAlbumForm;
