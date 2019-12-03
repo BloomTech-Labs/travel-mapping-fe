c153a7ddaad062a26c384197834ef0695c3f960a
arturolei <artie.lei@gmail.com>
2019-12-02 22:38:00 -0500

    Tried to make individual album data match what API documentation says are the keys/properties of each album

--- piktorlog/src/components/molecules/AlbumCard.js
+++ piktorlog/src/components/molecules/AlbumCard.js
@@ -6,7 +6,7 @@ const AlbumCard = (props) => (
         <Card.Content>
             <Image src ={props.src}/>
             <Card.Header>{props.title}</Card.Header>
-            <Card.Descripton>{props.caption.slice(150)+'...'}</Card.Descripton>
+            <Card.Descripton>{props.description.slice(150)+'...'}</Card.Descripton>
         </Card.Content>
         <Card.Content extra>
             <Button>Details</Button>
