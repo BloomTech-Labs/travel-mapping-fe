4a9e31d3839afd38ec062dff4c625316aa7f7bd4
arturolei <artie.lei@gmail.com>
2019-12-09 20:39:49 -0800

    Added cover_url/thumbnail but left commented out; feature should work once backend master branch is updated

--- piktorlog/src/components/molecules/AlbumCard.js
+++ piktorlog/src/components/molecules/AlbumCard.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const AlbumCard = (props) => (
     <Card>
         <Card.Content>
-            {/* <Image src ={props.src}/> */}
+            {/* <Image src = {props.albumData.cover_url}/> */}
             <Card.Header>{props.albumData.title}</Card.Header>
             <p>{props.albumData.description+'...'}</p>
         </Card.Content>
