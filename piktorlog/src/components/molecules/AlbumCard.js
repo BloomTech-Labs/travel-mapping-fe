24d1e146e0ca9cae1d6983405bf477d530e4173e
arturolei <artie.lei@gmail.com>
2019-12-12 19:01:52 -0800

    Tested if image path works

--- piktorlog/src/components/molecules/AlbumCard.js
+++ piktorlog/src/components/molecules/AlbumCard.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 const AlbumCard = (props) => (
     <Card>
         <Card.Content>
+        <Image src = "https://upload.wikimedia.org/wikipedia/commons/7/72/Detail_of_the_Central_Branch_of_Greater_Victoria_Public_Library%2C_Victoria%2C_British_Columbia%2C_Canada_01.jpg"/>
             {/* <Image src = {props.albumData.cover_url}/> */}
             <Card.Header>{props.albumData.title}</Card.Header>
             <p>{props.albumData.description+'...'}</p>
