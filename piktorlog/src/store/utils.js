33ddec3d83c69dc2677cb6df7a9dc0e143d8a470
adrian <adrianadames@gmail.com>
2020-01-03 18:59:28 -0500

    Changed value of backend API url in utils ('address') to our staging API url for trial and error testing with UIUX I'll work on for viewing media in an album.

--- piktorlog/src/store/utils.js
+++ piktorlog/src/store/utils.js
@@ -1,7 +1,7 @@
 import ls from 'local-storage';
 
-export const address = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://piktorlog.herokuapp.com';
-// export const address = 'https://piktorlogstaging.herokuapp.com';
+// export const address = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://piktorlog.herokuapp.com';
+export const address = 'https://piktorlogstaging.herokuapp.com';
 export const getToken = () => {
   const user = ls.get('user');
 
