2ecfa43e4f83ed750e145c45300d22a7a0239d78
arturolei <artie.lei@gmail.com>
2020-01-06 20:28:42 -0800

    Initial commit, adding JS files and setting up layout of collaborators form

---
+++ piktorlog/src/components/molecules/AddCollaboratorForm.js
@@ -0,0 +1,2 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Segment, Button, Header, Grid, GridColumn, Icon } from 'semantic-ui-react';
---
+++ piktorlog/src/pages/CollaboratorsOverview.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+
+const CollaboratorsOverview = (props) => {
+
+
+    return (
+        <React.fragment>
+            <div>
+                <p>Coming Soon</p>
+            </div>
+        </React.fragment>
+    )
+};
+
+export default CollaboratorsOverview;
