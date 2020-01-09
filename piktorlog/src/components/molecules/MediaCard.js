13636682a405d4f659564093940a0bb0803419f7
adrian <adrianadames@gmail.com>
2020-01-08 22:56:48 -0500

    Centered media item in modal. Styled modal background. Put info button on top left of media item when it pops up as modal.

--- piktorlog/src/components/molecules/MediaCard.js
+++ piktorlog/src/components/molecules/MediaCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Card, Image, Dropdown, DropdownItem, Modal} from 'semantic-ui-react';
+import {Button, Card, Image, Dropdown, DropdownItem, Modal, Popup} from 'semantic-ui-react';
 import { Redirect, Link } from 'react-router-dom';
 
 import styled from 'styled-components';
@@ -14,7 +14,7 @@ const StyledCardContent = styled(Card.Content)({
     'z-index':'0!important',
 });
 
-const StyledDropdown = styled(Dropdown)({
+const StyledDropdownTransparent = styled(Dropdown)({
     position: 'absolute!important',
     top:'0',
     right:'0',
@@ -22,44 +22,88 @@ const StyledDropdown = styled(Dropdown)({
     background:'rgba(224,225,226,.4)!important'
 });
 
+const StyledDropdownOpaque = styled(Dropdown)({
+    position: 'absolute!important',
+    top:'0',
+    right:'0',
+    'z-index':'1!important',
+    background:'rgba(224,225,226,0.6)!important'
+});
+
+const StyledModalContent = styled(Modal.Content)({
+    // border: '1px solid red',
+    position:'relative',
+    'z-index':'0!important',
+    background:'rgba(38,38,38,0.15)!important',
+    // background:'rgba(0,0,0,0.1)!important'
+});
+
+const StyledPopup = styled(Popup)({
+    // position: 'absolute!important',
+    // top:'0',
+    // left:'0',
+    // 'z-index':'2!important',
+    // border:'2px solid red!important'
+    // background:'rgba(224,225,226,.4)!important'
+
+});
 
 
 const MediaCard = ({mediaItem}) => {
     console.log('mediaItem: ', mediaItem);
     console.log('Object.entries(mediaItem): ', Object.entries(mediaItem));
-
+ 
     return (
         <Card>
             <Card.Content>
-                <Modal trigger = {
-                <StyledCardContent>
-                    {/* Dropdown Menu */}
-                    <StyledDropdown
-                        button
-                        icon = 'ellipsis vertical' 
-                    >
-                        <Dropdown.Menu>
-                            <Dropdown.Item text = 'edit' /> 
-                            <Dropdown.Item text = 'delete' />
-                        </Dropdown.Menu>
-                    </StyledDropdown>
+                <Modal 
+                    trigger = {
+                        <StyledCardContent>
+                            {/* Dropdown Menu */}
+                            <StyledDropdownTransparent
+                                button
+                                icon = 'ellipsis vertical' 
+                            >
+                                <Dropdown.Menu>
+                                    <Dropdown.Item text = 'edit' /> 
+                                    <Dropdown.Item text = 'delete' />
+                                </Dropdown.Menu>
+                            </StyledDropdownTransparent>
 
-                    {/* Image */}
-                    <Image src = {mediaItem.media_url} />
-                </StyledCardContent>}>
+                            {/* Image */}
+                            <Image src = {mediaItem.media_url} />
+                        </StyledCardContent>
+                    }
+                    style = {{background: 'rgba(224,225,226,.4)'}}
+                    size = 'large'
+                >
 
-                    <Modal.Content image>
-                        <StyledDropdown
-                            button
-                            icon = 'ellipsis vertical' 
-                        >
-                            <Dropdown.Menu>
-                                <Dropdown.Item text = 'edit' /> 
-                                <Dropdown.Item text = 'delete' />
-                            </Dropdown.Menu>
-                        </StyledDropdown>
-                        <Image wrapped size = 'fullscreen' src = {mediaItem.media_url} /> 
-                    </Modal.Content>
+                    <StyledModalContent image>
+                        <div style={{position:'relative', margin:'auto',}}>
+                            <StyledDropdownOpaque
+                                button
+                                icon = 'ellipsis vertical' 
+                            >
+                                <Dropdown.Menu>
+                                    <Dropdown.Item text = 'edit' /> 
+                                    <Dropdown.Item text = 'delete' />
+                                </Dropdown.Menu>
+                            </StyledDropdownOpaque>
+                            
+                            <Image wrapped size = 'fullscreen' src = {mediaItem.media_url} />
+                            <StyledPopup 
+                                position='bottom left'
+                                trigger={
+                                    <Button icon='info circle' style={{position:'absolute', left:0, top:0, background:'rgba(224,225,226,.6)'}}/>
+                                }
+                            >
+                                <Popup.Content>
+                                    Media Info:  
+                                </Popup.Content>
+                            </StyledPopup>
+                        </div>                       
+                    </StyledModalContent>
+
                 </Modal>              
             </Card.Content>
         </Card>
