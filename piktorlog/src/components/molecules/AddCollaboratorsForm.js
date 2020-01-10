358e3fe8ab6230c06fff06f3417dd94d698ebf57
arturolei <artie.lei@gmail.com>
2020-01-09 20:09:23 -0800

    Made some adjustments

--- piktorlog/src/components/molecules/AddCollaboratorsForm.js
+++ piktorlog/src/components/molecules/AddCollaboratorsForm.js
@@ -9,10 +9,18 @@ Form will have really 1 field: takes email address as input --> Make a call that
 */
 
 const AddCollaboratorsForm = (props) =>  {
+    const [email, setEmail] = useState('');
     return (
         <Form>
             <Header>Add Collaborator</Header>
-
+            <Form.Input
+                fluid
+                icon="mail"
+                iconPosition="left"
+                placeholder="Collaborator E-mail address"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
         </Form>
     )
 }
