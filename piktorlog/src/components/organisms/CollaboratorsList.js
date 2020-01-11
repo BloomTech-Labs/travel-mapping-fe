18a2a59b17eae2d4881b3dd68bbeeeca2d243885
Tim Dill <brodt7258@gmail.com>
2020-01-11 03:31:48 -0500

    display collabs, option to remove, with confirmation modal

--- piktorlog/src/components/organisms/CollaboratorsList.js
+++ piktorlog/src/components/organisms/CollaboratorsList.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-import { useGetCollabAlbum, useLogOnChange } from '../../store/requests/hooks';
+import { Button, Modal } from 'semantic-ui-react';
+
+import { useGetCollabAlbum, useLogOnChange, useRemoveCollab } from '../../store/requests/hooks';
 
 const CollaboratorsList = ({ album_id }) => {
 
-  const [collaborators] = useGetCollabAlbum(album_id);
-  useLogOnChange(`album ${album_id} collabs:`, collaborators)
+  const [collaborators, , , refresh] = useGetCollabAlbum(album_id);
+  useLogOnChange(`album ${album_id} collabs:`, collaborators);
+
+  const [removeCollab] = useRemoveCollab(refresh);
 
   return (
-    <div>collabList</div>
-  )
+    <div>
+      collabList
+      <ul>
+        {collaborators.map(e => (
+          <li key={e.collaborator_id}>
+            <span>{e.display_name}</span>
+            {/* <Button onClick={() => removeCollab(album_id, e.collaborator_id)}>
+              Remove
+            </Button> */}
+            <Modal 
+              trigger={<Button>Remove</Button>}
+              header={`Remove ${e.display_name} as a Collaborator?`}
+              content="Are you sure? You'll have to send them a new invitation if you want them back"
+              actions={['Cancel', {key: 'remove', content: 'Remove', negative: true, value: 'remove'}]}
+              onActionClick={(event) => { if (event.target.value === 'remove') removeCollab(album_id, e.collaborator_id) }}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
 };
 
