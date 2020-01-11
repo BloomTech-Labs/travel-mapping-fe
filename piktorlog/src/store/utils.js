aa327ab3b740035e3b96dca4d838cf53a77da539
adrian <adrianadames@gmail.com>
2020-01-10 20:11:15 -0500

    Made function for converting the format of date and time stored in our backend to the local time of the user.

--- piktorlog/src/store/utils.js
+++ piktorlog/src/store/utils.js
@@ -1,7 +1,7 @@
 import ls from 'local-storage';
 
-// export const address = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://piktorlog.herokuapp.com';
-export const address = 'https://piktorlogstaging.herokuapp.com';
+export const address = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://piktorlog.herokuapp.com';
+// export const address = 'https://piktorlogstaging.herokuapp.com';
 export const getToken = () => {
   const user = ls.get('user');
 
@@ -16,4 +16,14 @@ export const createAuthHeader = () => {
   const token = getToken();
 
   return token ? { headers: { Authorization: `Bearer ${token}` }} : null;
+};
+
+
+// format of DateTimeString is same format as the knex created_at date-time format
+// example of DateTimeString: '2020-01-11T00:19:29.571Z'
+// example of associated output: '1/10/2020, 7:19:29 PM'
+export const getLocalDateAndTime = (dateTimeString) => {
+  const dateTimeObj = new Date(dateTimeString);
+  const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  return dateTimeObj.toLocaleString({timezone:String(userTimezone)});
 };
