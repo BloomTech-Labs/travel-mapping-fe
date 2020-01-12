dd117b37760cd5648d714e83ea3fd8edd554eaf6
Tim Dill <brodt7258@gmail.com>
2020-01-12 06:29:27 -0500

    display invites, allow invite decline

--- piktorlog/src/components/organisms/UserInvites.js
+++ piktorlog/src/components/organisms/UserInvites.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
+import { List, Image, Button } from 'semantic-ui-react';
+
 import { useGetInvitesToUser } from '../../store/hooks/useImmediateFetch';
+import { useCancelInvite } from '../../store/hooks/useFetchOnRequest';
 import { useLogOnChange } from '../../store/hooks/misc';
 
 const UserInvites = ({ user_id }) => {
 
-  const [invites] = useGetInvitesToUser(user_id);
+  const [invites, , , refreshInvites] = useGetInvitesToUser(user_id);
   useLogOnChange('invites', invites);
 
+  const [cancelInvite] = useCancelInvite(refreshInvites);
+
   return (
-    <div>UserInvites</div>
-  )
+    <div>
+    <h3>UserInvites</h3>
+    <List>
+        {invites.map(e => (
+          <List.Item key={e.album_id}>
+            <Image size="mini" inline src={e.cover_url} />
+            <List.Content>
+              <List.Header>{e.title}</List.Header>
+              <List.Description>
+                {e.description}
+              </List.Description>
+            </List.Content>
+            <Button onClick={() => cancelInvite(e.invitation_id)}>
+              Decline
+            </Button>
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  );
 
 };
 
